0503963bd81c9477807e6561837fd841a04bf99e
malobr <marcelotc1202@gmail.com>
2024-05-20 19:19:33 -0300

    Section Header pronta

--- Gerich-Restaurant/my-app/src/container/Header/Header.jsx
+++ Gerich-Restaurant/my-app/src/container/Header/Header.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 import {images} from '../../constants';
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div className='app__wrapper section__padding' id='home'>
-    <div>
+  <div className='app__header app__wrapper section__padding' id='home'>
       <div className='app__wrapper_info'>
-
+        <SubHeading  title="Chase the new Flavour"/>
+        <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+        <p className='p__opensans' style={{margin: '2rem 0'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio possimus, a unde enim magni alias accusamus nisi adipisci nihil natus quis nesciunt! Nam quaerat dignissimos minima rerum, natus laudantium voluptates.</p>
+        <button type="button" className='custom__button'>Explore More</button>
       </div>
       <div className='app__wrapper_img'>
-
+        <img src={images.welcome} alt="heder img" srcset="" />
       </div>
-    </div>
   </div>
 );
 
